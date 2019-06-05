669cdc8425426d94b90459b0c4e9196fd15b025d
git <liuying880209@163.com>
2019-06-05 17:55:20 +0800

    回答问题记录

---
+++ src/main/java/com/farm/core/farm/service/AnswersRecordService.java
@@ -0,0 +1,9 @@
+package com.farm.core.farm.service;
+
+import com.farm.core.farm.AnswersRecordVO;
+import com.farm.core.farm.AnswersVO;
+
+public interface AnswersRecordService {
+
+   Integer addAnswersRecord(AnswersRecordVO answersRecordVO);
+}
